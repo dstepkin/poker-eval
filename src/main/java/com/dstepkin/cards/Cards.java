559a51372b4a94ee31395bec1ded663baa917364
Denis Stepkin <denis.stepkin.ru@gmail.com>
2020-05-30 17:22:26 +0300

    updated javadoc

--- src/main/java/com/dstepkin/cards/Cards.java
+++ src/main/java/com/dstepkin/cards/Cards.java
@@ -4,11 +4,11 @@ public interface Cards {
 
     /**
      * every card is a single bit.
-     * Hand ranks:    Two   Thre Four Five Six   Seve Eigh Nine Ten   Jack Quee King Ace
-     * 0000 0000 0000 0000  0000 0000 0000 0000  0000 0000 0000 0000  0000 0000 0000 0000
-     * Hand suits:    cdhs  cdhs cdhs cdhs cdhs  cdhs cdhs cdhs cdhs  cdhs cdhs cdhs cdhs
+     * Card ranks:    Two   Thre Four Five Six   Seve Eigh Nine Ten   Jack Quee King Ace
+     * 0000 0000 0000 1111  1111 1111 1111 1111  1111 1111 1111 1111  1111 1111 1111 1111
+     * Card suits:    cdhs  cdhs cdhs cdhs cdhs  cdhs cdhs cdhs cdhs  cdhs cdhs cdhs cdhs
      *
-     * @return list of cards
+     * @return list of cards as a single long number
      */
     long getCards();
 
